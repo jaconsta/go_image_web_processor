b01d378216a8e4b9601d3eaedefbff964ff4bfdf
Javier constain <constainopia@gmail.com>
2018-07-04 11:08:44 -0500

    Load images from files paths

--- main.go
+++ main.go
@@ -3,19 +3,41 @@ package main
 import (
 	// "bytes"
 	// "encoding/base64"
+	"io/ioutil"
 	"fmt"
 	"image"
 	"image/png"
 	"image/jpeg"
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/nfnt/resize"
 )
 
-func loadFromFile(filePath string) (img image.Image, err error) {
+var sourceFolder = "./source_images"
+var destinationFolder = "./processed_images"
+
+func readFolderFiles(folderPath string) ([]string, error) {
+	// Try Glob too. https://golang.org/src/path/filepath/match.go?s=5609:5664#L224
+	files, err := ioutil.ReadDir(folderPath)
+    if err != nil {
+        return nil, err
+    }
+
+		var fileNames []string
+    for _, f := range files {
+			if !f.IsDir() && f.Name() != ".gitkeep"{
+				fileNames = append(fileNames, f.Name())
+			}
+    }
+		return fileNames, nil
+}
+
+func loadFromFile(fileName string) (img image.Image, err error) {
+	fileLocation := filepath.Join(sourceFolder, fileName)
 	// Load File
-	storedFile, err := os.Open(filePath)
+	storedFile, err := os.Open(fileLocation)
 	if err != nil {
 		return nil, fmt.Errorf("Could not load file")
 	}
@@ -34,14 +56,14 @@ func loadFromFile(filePath string) (img image.Image, err error) {
 		if err != nil {
 			return nil, fmt.Errorf("Could not load (Decode) file to image")
 		}
-	}
-	if imageType == "jpeg" {
+	}	else if imageType == "jpeg" {
 		img, err = jpeg.Decode(storedFile)
 		if err != nil {
 			return nil, fmt.Errorf("Could not load (Decode) file to image")
 		}
+	}	else {
+		return nil, nil
 	}
-
 
 	return  img, nil
 }
@@ -63,7 +85,8 @@ func loadFromString(base64String string) (img image.Image, err error) {
 }
 
 func saveToFile(img image.Image, filename string) error {
-	outputImage, err := os.Create(filename)
+	fileLocation := filepath.Join(destinationFolder, filename)
+	outputImage, err := os.Create(fileLocation)
 	if err != nil {
 		return fmt.Errorf("Could not create destination file.")
 	}
@@ -74,16 +97,22 @@ func saveToFile(img image.Image, filename string) error {
 }
 
 func main() {
-	encodedImage, err := loadFromFile("cuadro02.jpg")
+	imageList, err := readFolderFiles(sourceFolder)
 	if err != nil {
 		log.Fatal(err)
 	}
+	for _, fileName := range imageList {
+		encodedImage, err := loadFromFile(fileName)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	var maxWidth uint = 200
-	var maxHeight uint = 0  // To preserve aspect ratio 
-	thumbnail := resize.Resize(maxWidth, maxHeight, encodedImage, resize.Lanczos3)
-	err = saveToFile(thumbnail, "three_copy.png")
-	if err != nil {
-		log.Fatal(err)
+		var maxWidth uint = 200
+		var maxHeight uint = 0  // To preserve aspect ratio
+		thumbnail := resize.Resize(maxWidth, maxHeight, encodedImage, resize.Lanczos3)
+		err = saveToFile(thumbnail, "three_copy.png")
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 }
